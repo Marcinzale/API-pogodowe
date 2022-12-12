e08f2e7416c116ec5e72f131012293c697dfeea4
Marcin <marcinzale@gmail.com>
2022-12-12 19:58:07 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from requests import get
+from json import loads
+from terminaltables import AsciiTable
+
+def main():
+    CITY = input("Podaj miasto: ")
+    url = 'https://danepubliczne.imgw.pl/api/data/synop'
+    response = get(url)
+    rows = [
+        ['Miasto', 'Godzina pomiaru', 'Temperatura', 'Ciśnienie']
+    ]
+
+    for row in loads(response.text):
+        if row['stacja'] == CITY:
+            rows.append([
+                row['stacja'],
+                row['godzina_pomiaru'],
+                row['temperatura'],
+                row['cisnienie']
+            ])
+
+    table = AsciiTable(rows)
+    print(table.table)
+    answer = str.lower(input("Do you want to exit? Enter Y or N: "))
+    if answer == 'n':
+        main()
+    else:
+        print("Goodbye")
+        quit()
+
+if __name__ == '__main__':
+    main()
+
+
+
